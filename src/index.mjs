bcbd757e726e2b821cef20e5a1112ec212599480
jasmineblinks <puenehfaith@gmail.com>
2022-05-08 05:29:30 +0100

    feat: updated the index.mjs file wih a new feature

--- src/index.mjs
+++ src/index.mjs
@@ -8,7 +8,7 @@ import * as backend from "./build/index.main.mjs";
 import { loadStdlib } from "@reach-sh/stdlib";
 const reach = loadStdlib(process.env);
 
-const handToInt = { ROCK: 0, PAPER: 1, SCISSORS: 2 };
+const handToInt = { X: 0, Neutral: 1, O: 2 };
 const intToOutcome = ["Bob wins!", "Draw!", "Alice wins!"];
 const { standardUnit } = reach;
 const defaults = { defaultFundAmt: "10", defaultWager: "3", standardUnit };
